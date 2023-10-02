c876103210c56c35a3e0e1f10d028a19a5db46d9
David Grammatico <david.lugdunum@gmail.com>
2023-10-02 22:56:40 +0200

    add searchBar

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,7 +1,39 @@
 import {Text, View, Image, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import Colors  from '@/constants/Colors'
+import { Link } from 'expo-router'
+import { TextInput } from 'react-native-gesture-handler'
+
+const SearchBar = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleReset = () => {
+    setSearchText('');
+  };
+  return (
+
+    <View style={styles.searchContainer}>
+      <View style={styles.searchSection}>
+        <View style={styles.searchField}>
+          <Ionicons style={styles.searchIcon} name="ios-search" size={20} color={Colors.medium} />
+          <TextInput style={styles.input} placeholder="Restaurants, groceries, dishes" onChangeText={setSearchText} value={searchText} />
+          {searchText !== '' && (
+            <TouchableOpacity onPress={handleReset}>
+              <Ionicons name="close-circle-outline" size={20} color={Colors.medium} />
+            </TouchableOpacity>
+          )}
+        </View>
+        <Link href={'/'} asChild>
+          <TouchableOpacity style={styles.optionButton}>
+            <Ionicons name="options-outline" size={20} color={Colors.primary} />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  )
+}
+
 
 const CustomHeader = () => {
   return (
@@ -21,6 +53,7 @@ const CustomHeader = () => {
           <Ionicons name="person-outline" size={20} color={Colors.primary}/>
         </TouchableOpacity>
       </View>
+      <SearchBar />
     </SafeAreaView>
   )
 }
@@ -63,6 +96,39 @@ const styles = StyleSheet.create({
   profileButton: {
     padding: 10
   },
+  searchContainer: {
+    height: 60,
+    backgroundColor: '#fff',
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+  },
+  optionButton: {
+    padding: 10,
+  },
+  input: {
+    fontSize: 14,
+    flex: 1,
+    color: Colors.mediumDark,
+    textAlign: "left",
+    padding: 10,
+  },
+  searchIcon: {
+    // paddingLeft: 10,
+  }
 })
 
 export default CustomHeader
